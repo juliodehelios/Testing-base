0e71b31db2fd8ecb05425f52177f5df1e10d081b
Marketer <110580756+juliodehelios@users.noreply.github.com>
2024-05-20 19:01:11 +0100

    Add files via upload

---
+++ tblAnnStatus.sql
@@ -0,0 +1,17 @@
+/*
+SQLyog Trial v13.1.8 (64 bit)
+MySQL - 8.0.19-10 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `tblannstatus` (
+	`AnnStatID` int (11),
+	`StatName` varchar (60)
+); 
+insert into `tblannstatus` (`AnnStatID`, `StatName`) values('0','Deleted Text');
+insert into `tblannstatus` (`AnnStatID`, `StatName`) values('1','Found');
+insert into `tblannstatus` (`AnnStatID`, `StatName`) values('2','Downloaded');
+insert into `tblannstatus` (`AnnStatID`, `StatName`) values('3','Categorised');
+insert into `tblannstatus` (`AnnStatID`, `StatName`) values('4','Processed');
+insert into `tblannstatus` (`AnnStatID`, `StatName`) values('5','Part-Processed');
