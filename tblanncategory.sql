ea82c69f0ac0cdefa3ead598d72ba9d12ea8b0dd
Marketer <110580756+juliodehelios@users.noreply.github.com>
2024-05-20 19:00:21 +0100

    Add files via upload

---
+++ tblanncategory.sql
@@ -0,0 +1,26 @@
+/*
+SQLyog Trial v13.1.8 (64 bit)
+MySQL - 8.0.19-10 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `tblanncategory` (
+	`ItemId` int (11),
+	`CatName` varchar (150)
+); 
+insert into `tblanncategory` (`ItemId`, `CatName`) values('1','Uncategorised');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('6','Shareholding change');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('8','Unusable');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('9','Director change');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('10','Not reportable');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('11','Option Grant');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('12','Shares on Issue');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('13','New Company');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('14','Buyback');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('15','Intention to Buyback');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('1002','Clarification Pending');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('1003','Transaction Narrow');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('1004','Proxy');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('1005','Pledged Data');
+insert into `tblanncategory` (`ItemId`, `CatName`) values('1006','144 - PreAnnouncement');
